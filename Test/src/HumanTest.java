1551234f144c87071c18a06a6651964a5e6a15cb
amichikawa11 <amichikawa11@gmail.com>
2017-12-19 16:38:57 +0900

    12/19

---
+++ Test/src/HumanTest.java
@@ -0,0 +1,15 @@
+
+public class HumanTest {
+	public static void main(String[]args){
+		
+		
+		Human human1 = new Human("nobuo",19770101);
+		
+		human1.setName("アヤノコウジ　ノブゴロウ");
+		human1.setName("ノブ");
+		
+		System.out.println(human1.getName());
+
+		
+	}
+}
