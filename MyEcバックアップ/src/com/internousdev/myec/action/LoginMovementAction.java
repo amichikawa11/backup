6e849e9b10ca95b9d0461630d8a3d3f9f1879949
bbhne <hnba225@gmail.com>
2018-02-01 16:27:52 +0900

    0201

---
+++ MyEcバックアップ/src/com/internousdev/myec/action/LoginMovementAction.java
@@ -0,0 +1,9 @@
+package com.internousdev.myec.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginMovementAction extends ActionSupport {
+	public String execute(){
+		return SUCCESS;
+	}
+}
