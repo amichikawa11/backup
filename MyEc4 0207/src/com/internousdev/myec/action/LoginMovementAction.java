fa91f987dcd61ce7f3751473cbdb608ed725558f
amichikawa11 <amichikawa11@gmail.com>
2018-02-07 17:50:34 +0900

    0207-2

--- MyEc4 0207/src/com/internousdev/myec/action/LoginMovementAction.java
+++
@@ -1,9 +0,0 @@
-package com.internousdev.myec.action;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class LoginMovementAction extends ActionSupport {
-	public String execute(){
-		return SUCCESS;
-	}
-}
